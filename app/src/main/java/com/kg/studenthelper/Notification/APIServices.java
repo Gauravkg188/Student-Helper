751c28fcc271cbc2181311b259567005a8013ebb
Gauravkg188 <71400260+Gauravkg188@users.noreply.github.com>
2021-02-17 22:08:51 +0530

    Update APIServices.java

--- app/src/main/java/com/kg/studenthelper/Notification/APIServices.java
+++ app/src/main/java/com/kg/studenthelper/Notification/APIServices.java
@@ -10,7 +10,7 @@ public interface APIServices {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAFwLtv50:APA91bGIKz6U8E6wCs6xBKp6y6xWnSyaBky-YAt_w4FWMDRRmq1BBMC1Mzh3lS77aw7esA6liisc3cval7DjAUvX1VX8L0iwNU_9NmcQg2bri6hQm5vXWyV-tTO6VoN7z4wDqe8NsgNA"
+                    "Authorization:key=API_KEY"
             }
     )
 
